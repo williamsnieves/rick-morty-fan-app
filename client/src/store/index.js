8945559701cba25c228f7d53cf8832621378518d
Willians Nieves <williamsnieves@gmail.com>
2022-07-07 08:38:30 +0200

    feat: remove comments

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,15 +3,6 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reduxThunk from "redux-thunk";
 import reducers from "../reducers";
 
-const toolsEnhancer =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-/*const createStoreWithMiddleware = applyMiddleware(
-  reduxThunk,
-  enhancer
-)(createStore);
-const store = createStoreWithMiddleware(reducers);*/
-
 const middlewareEnhancer = applyMiddleware(reduxThunk);
 const composedEnhancers = compose(middlewareEnhancer);
 
